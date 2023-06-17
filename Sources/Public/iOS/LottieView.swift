a89f0459b914f02a63940f7643d286bcfbf0af59
vdotup <v.up@live.com>
2023-06-17 22:26:53 +0300

    .

--- Sources/Public/iOS/LottieView.swift
+++ Sources/Public/iOS/LottieView.swift
@@ -7,18 +7,17 @@
 
 import SwiftUI
 /// SwiftUI UIViewRepresentable for LottieAnimationView.
+@available(iOS 13.0, *)
 public struct LottieView: UIViewRepresentable {
     
+    @ObservedObject private var helper = LottieViewHelper()
+    
     public let name: String
-    public let loopMode: LottieLoopMode
-    public let animationSpeed: CGFloat
     public let contentMode: UIView.ContentMode
     public let animationView: LottieAnimationView
     
-    public init(name: String, loopMode: LottieLoopMode = .loop, animationSpeed: CGFloat = 1, contentMode: UIView.ContentMode = .scaleAspectFit) {
+    public init(name: String, contentMode: UIView.ContentMode = .scaleAspectFit) {
         self.name = name
-        self.loopMode = loopMode
-        self.animationSpeed = animationSpeed
         self.contentMode = contentMode
         self.animationView = LottieAnimationView(name: name)
     }
@@ -31,8 +30,8 @@ public struct LottieView: UIViewRepresentable {
         animationView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
         animationView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         
-        animationView.loopMode = loopMode
-        animationView.animationSpeed = animationSpeed
+        animationView.loopMode = helper.loopMode
+        animationView.animationSpeed = helper.animationSpeed
         animationView.play()
         
         return view
@@ -40,4 +39,23 @@ public struct LottieView: UIViewRepresentable {
     
     public func updateUIView(_ uiView: UIView, context: Context) {}
     
+}
+
+@available(iOS 13.0, *)
+extension LottieView {
+    
+    class LottieViewHelper: ObservableObject {
+        @Published internal var loopMode: LottieLoopMode = .loop
+        @Published internal var animationSpeed: CGFloat = 1.0
+    }
+    
+    public func loopMode(_ loopMode: LottieLoopMode = .loop) -> Self {
+        self.helper.loopMode = loopMode
+        return self
+    }
+    
+    public func animationSpeed(_ animationSpeed: CGFloat = 1.0) -> Self {
+        self.helper.animationSpeed = animationSpeed
+        return self
+    }
 }
